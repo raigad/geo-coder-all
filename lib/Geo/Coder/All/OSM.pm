0158af37c8efb76886ddd55517e655d93f75c82b
Rohit <rohitdeshmukh69@gmail.com>
2014-03-22 00:19:22 +0000

    added generic output structure for the OSM

--- lib/Geo/Coder/All/OSM.pm
+++ lib/Geo/Coder/All/OSM.pm
@@ -4,6 +4,7 @@ use Moose;
 use namespace::autoclean;
 use Carp;
 use Geo::Coder::OSM;
+use Data::Dumper;
 with 'Geo::Coder::Role::Geocode';
 has 'OSM' => (
     is      =>  'ro',
@@ -14,7 +15,14 @@ has 'OSM' => (
 sub geocode_local{
     my ($self,$rh_args) = @_;
     croak "Adress needed" unless ($rh_args->{address});    
-    return $self->OSM->geocode(location => $rh_args->{address}); 
+    my $rh_response = $self->OSM->geocode(location => $rh_args->{address});
+    my $rh_data;
+    $rh_data->{address}    = $rh_response->{display_name};
+    $rh_data->{coordinates}{lat} = $rh_response->{lat};
+    $rh_data->{coordinates}{lon} = $rh_response->{lon};
+    $rh_data->{country}         = $rh_response->{address}{country};
+    $rh_data->{country_code}    = uc($rh_response->{address}{country_code});
+    return $rh_data;
 }
 __PACKAGE__->meta->make_immutable;
 1;
