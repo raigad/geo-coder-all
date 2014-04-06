40c3b5cbc959655f3b4dd4b25f68e27e0bf5ebb3
Rohit <rohitdeshmukh69@gmail.com>
2014-04-06 21:08:04 +0100

    updated docs for release

--- lib/Geo/Coder/All.pm
+++ lib/Geo/Coder/All.pm
@@ -71,7 +71,7 @@ sub _process_args {
 
 =head1 NAME
 
-Geo::Coder::All - The great new Geo::Coder::All!
+Geo::Coder::All - Geo::Coder::All
 
 =head1 VERSION
 
@@ -82,9 +82,11 @@ Version 0.01
 our $VERSION = '0.01';
 
 
-=head1 SYNOPSIS
+=head1 DESCRIPTION
 
 Geo::Coder::All is wrapper for other geocoder cpan modules such as Geo::Coder::Google,Geo::Coder::Bing,Geo::Coder::Ovi,Geo::Coder::OSM and Geo::Coder::TomTom. Geo::Coder::All provides common geocode output format for all geocoder.
+
+=head1 SYNOPSIS
 
     use Geo::Coder::All;
     #For google geocoder
@@ -106,8 +108,7 @@ Geo::Coder::All is wrapper for other geocoder cpan modules such as Geo::Coder::G
 
 =head1 METHODS
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+Geo::Coder::All offers geocode and reverse_geocode methods
 
 =over 2
 
@@ -136,7 +137,12 @@ For Google reverse_geocoder
     $rh_location = $google_geocoder->reverse_geocode({latlng=>'51.508515,-0.1254872',language=>'es',encoding=>'utf8',sensor=>1})
     
 =cut
+=back
 
+=head1 SEE ALSO
+=over 1
+=item geocoders
+    Geo::Coder::Many,Geo::Coder::Google,Geo::Coder::Bing,Geo::Coder::Ovi,Geo::Coder::OSM and Geo::Coder::TomTom
 =back
 
 =head1 AUTHOR
@@ -184,47 +190,9 @@ L<http://search.cpan.org/dist/Geo-Coder-All/>
 
 =head1 ACKNOWLEDGEMENTS
 
-
 =head1 LICENSE AND COPYRIGHT
 
 Copyright 2014 Rohit Deshmukh.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of the the Artistic License (2.0). You may obtain a
-copy of the full license at:
-
-L<http://www.perlfoundation.org/artistic_license_2_0>
-
-Any use, modification, and distribution of the Standard or Modified
-Versions is governed by this Artistic License. By using, modifying or
-distributing the Package, you accept this license. Do not use, modify,
-or distribute the Package, if you do not accept this license.
-
-If your Modified Version has been derived from a Modified Version made
-by someone other than you, you are nevertheless required to ensure that
-your Modified Version complies with the requirements of this license.
-
-This license does not grant you the right to use any trademark, service
-mark, tradename, or logo of the Copyright Holder.
-
-This license includes the non-exclusive, worldwide, free-of-charge
-patent license to make, have made, use, offer to sell, sell, import and
-otherwise transfer the Package with respect to any patent claims
-licensable by the Copyright Holder that are necessarily infringed by the
-Package. If you institute patent litigation (including a cross-claim or
-counterclaim) against any party alleging that the Package constitutes
-direct or contributory patent infringement, then this Artistic License
-to you shall terminate on the date that such litigation is filed.
-
-Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
-AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
-THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
-YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
-CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
-CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 
 =cut
 
